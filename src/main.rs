8bdef556efadcc8c4874987d29cc17577f51bb60
DrPeterVanNostrand <jnz@riseup.net>
2022-09-02 18:02:42 +0000

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use ff::PrimeField;
 use group::GroupEncoding;
 use halo2_proofs::{
-    arithmetic::{best_multiexp, lagrange_interpolate},
+    arithmetic::{best_multiexp, eval_polynomial, lagrange_interpolate},
     pasta::{Eq, EqAffine, Fp},
     poly::commitment::Params,
 };
@@ -12,7 +12,7 @@ type F = Fp;
 type C = Eq;
 type Affine = EqAffine;
 
-// Create Pedersen vector commitment params for vectors of len `n`.
+// Create Pedersen vector commitment params (without blinding) for vectors of len `n`.
 #[inline]
 fn gen_pedersen_params(n: usize) -> Vec<Affine> {
     assert!(n.is_power_of_two());
@@ -21,7 +21,7 @@ fn gen_pedersen_params(n: usize) -> Vec<Affine> {
     Params::new(k).get_g()
 }
 
-// Pedersen vector commitment.
+// Pedersen vector commitment (without blinding).
 #[inline]
 fn vec_commit(v: &[F], bases: &[Affine]) -> C {
     best_multiexp(v, bases)
@@ -64,44 +64,29 @@ impl<const A: usize> VerkleTree<A> {
 
         let interp_domain: Vec<F> = (0..A as u64).map(F::from).collect();
 
-        let mut cur_layer_len = num_leafs;
+        let mut cur_layer = leafs.clone();
         let mut non_leaf_layers = Vec::<Vec<NonLeafNode<A>>>::with_capacity(height);
 
-        for h in 0..height {
-            let next_layer_len = cur_layer_len >> log2_arity;
-            let mut next_layer = Vec::<NonLeafNode<A>>::with_capacity(next_layer_len);
-
-            if h == 0 {
-                for sibs in leafs.chunks(A) {
+        for _ in 0..height {
+            let next_layer: Vec<NonLeafNode<A>> = cur_layer
+                .chunks(A)
+                .map(|sibs| {
                     let poly_coeffs = lagrange_interpolate(&interp_domain, sibs);
-                    assert_eq!(poly_coeffs.len(), A);
+                    debug_assert_eq!(poly_coeffs.len(), A);
                     let commit = vec_commit(&poly_coeffs, pedersen_bases);
                     let commit_digest = hash_commit(&commit);
-                    next_layer.push(NonLeafNode {
+                    NonLeafNode {
                         poly_coeffs,
                         commit,
                         commit_digest,
-                    });
-                }
-            } else {
-                for sibs in non_leaf_layers.last().unwrap().chunks(A) {
-                    let sibs: Vec<F> = sibs.iter().map(|sib| sib.commit_digest).collect();
-                    let poly_coeffs = lagrange_interpolate(&interp_domain, &sibs);
-                    assert_eq!(poly_coeffs.len(), A);
-                    let commit = vec_commit(&poly_coeffs, pedersen_bases);
-                    let commit_digest = hash_commit(&commit);
-                    next_layer.push(NonLeafNode {
-                        poly_coeffs,
-                        commit,
-                        commit_digest,
-                    });
-                }
-            }
-
+                    }
+                })
+                .collect();
+            cur_layer = next_layer.iter().map(|node| node.commit_digest).collect();
             non_leaf_layers.push(next_layer);
-            cur_layer_len = next_layer_len;
         }
 
+        debug_assert_eq!(non_leaf_layers.last().unwrap().len(), 1);
         let root = non_leaf_layers[height - 1][0].commit;
 
         VerkleTree {
@@ -116,7 +101,7 @@ impl<const A: usize> VerkleTree<A> {
     }
 
     fn prove(&self, mut challenge: usize) -> VerkleProof<A> {
-        assert!(challenge < self.leafs.len());
+        debug_assert!(challenge < self.leafs.len());
         let leaf = self.leafs[challenge];
         let polys = self.non_leaf_layers
             .iter()
@@ -132,19 +117,6 @@ impl<const A: usize> VerkleTree<A> {
     }
 }
 
-#[inline]
-fn eval_poly(coeffs: &[F], x: F) -> F {
-    let mut x_pow = x;
-    coeffs
-        .iter()
-        .skip(1)
-        .fold(coeffs[0], |mut acc, coeff| {
-            acc += coeff * x_pow;
-            x_pow *= x;
-            acc
-        })
-}
-
 #[derive(Debug)]
 struct VerkleProof<const A: usize> {
     leaf: F,
@@ -156,27 +128,27 @@ impl<const A: usize> VerkleProof<A> {
         let arity_bit_len = A.trailing_zeros() as usize;
         
         // Check `poly_0(X)` evaluates to provided leaf.
+        let mut height = 0;
         let mut x = F::from((challenge % A) as u64);
-        let mut y = eval_poly(&self.polys[0], x);
+        let mut y = eval_polynomial(&self.polys[0], x);
         if y != self.leaf {
-            println!("error: poly_0(x) != leaf");
+            println!("error: poly_{}(x) != leaf", height);
             return false;
         }
         let mut commit = vec_commit(&self.polys[0], pedersen_bases);
 
         // Check `poly_i(X)` evaluates to the previous polynomial's commitment digest.
-        let mut height = 1;
         for poly in &self.polys[1..] {
+            height += 1;
             let commit_digest = hash_commit(&commit);
             challenge >>= arity_bit_len;
             x = F::from((challenge % A) as u64);
-            y = eval_poly(poly, x);
+            y = eval_polynomial(poly, x);
             if y != commit_digest {
                 println!("error: poly_{}(x) != commit(poly_{})", height, height - 1);
                 return false;
             }
             commit = vec_commit(poly, pedersen_bases);
-            height += 1;
         }
 
         commit == *root
